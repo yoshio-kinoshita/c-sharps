06ac6bffbd2f4f4956d17fa4013e49587fe90ae3
kinoshita.yoshio <kinoshita.yoshio@itcc.co.jp>
2014-04-18 16:54:08 +0900

    サンプル追加

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// Fackesを試すサンプル・アプリケーション
+namespace FackesSampleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /*
+         * 年度を返すメソッドです。
+         * 年度は4月～３月で区切ります。
+         */
+        private int GetYearCode()
+        {
+            DateTime now = new DateTime();
+
+            if (now.Month >= 4)
+            {
+                return now.Year;
+            }
+            else
+            {
+                return now.Year -1;
+            }
+        }
+
+
+    }
+}
